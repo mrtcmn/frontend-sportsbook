829f2eedc33fd5c2a80cb5859cfcf9497f5ad002
Murat Çimen <mrtcmen@gmail.com>
2023-07-08 23:52:12 +0300

    change: virtual scroll add

--- src/views/MainFixture.view.tsx
+++ src/views/MainFixture.view.tsx
@@ -2,12 +2,30 @@ import React, { Fragment } from 'react';
 import { useQuery } from '@tanstack/react-query';
 import FixtureRowComponent from '@src/components/FixtureRow.component';
 import { IFixtureItem } from '@src/dtos/fixture.dto';
+import { useVirtualizer } from '@tanstack/react-virtual';
 
 const MainFixtureView = () => {
+  const parentRef = React.useRef();
+
   const { isLoading, error, data } = useQuery({
     queryKey: ['betsData'],
     queryFn: () => fetch('https://nesine-case-study.onrender.com/bets').then((res) => res.json()),
   });
+
+  const getCount = (data: IFixtureItem[]) => {
+    if (data && Array.isArray(data)) {
+      return data.length;
+    }
+    return 0;
+  };
+
+  const rowVirtualizer = useVirtualizer({
+    count: getCount(data),
+    getScrollElement: () => parentRef.current,
+    estimateSize: () => 48,
+  });
+
+  const fixtureItemsV: any = rowVirtualizer.getVirtualItems();
 
   if (isLoading) {
     return <div className={'mainFixture-wrapper'}>Loading</div>;
@@ -20,12 +38,30 @@ const MainFixtureView = () => {
   return (
     <div className={'mainFixture-wrapper'}>
       <section className={'sticky-header'}>header</section>
-      <section className={'fixtureItems'}>
-        {data.map((item: IFixtureItem) => (
-          <Fragment key={item.NID}>
-            <FixtureRowComponent fixtureItem={item} />
-          </Fragment>
-        ))}
+      <section className={'fixtureItems'} ref={parentRef}>
+        <div
+          style={{
+            height: rowVirtualizer.getTotalSize(),
+            width: '100%',
+            position: 'relative',
+          }}
+        >
+          <div
+            style={{
+              position: 'absolute',
+              top: 0,
+              left: 0,
+              width: '100%',
+              transform: `translateY(${fixtureItemsV[0].start}px)`,
+            }}
+          >
+            {fixtureItemsV.map((item: any) => (
+              <div key={item.key} data-index={item.index} ref={item.measureElement}>
+                <FixtureRowComponent fixtureItem={data[item.index]} />
+              </div>
+            ))}
+          </div>
+        </div>
       </section>
     </div>
   );
