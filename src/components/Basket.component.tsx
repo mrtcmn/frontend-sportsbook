f47db819c1f431b26cac3af624b775075319be83
Murat Çimen <mrtcmen@gmail.com>
2023-07-09 20:01:12 +0300

    fix: basket style

--- src/components/Basket.component.tsx
+++ src/components/Basket.component.tsx
@@ -1,11 +1,12 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext, useEffect, useRef, useState } from 'react';
 import { BasketContext } from '@src/contexts/Basket.context';
 import { IBasketItem } from '@src/dtos/fixture.dto';
 
 const STAKE = 1;
 const BasketComponent = () => {
   const [totalAmount, setTotalAmount] = useState(0);
-  const { basketItems, setBasketItems } = useContext(BasketContext);
+  const { basketItems } = useContext(BasketContext);
+  const basketSectionRef: React.MutableRefObject<HTMLElement> = useRef<HTMLElement>(null);
 
   useEffect(() => {
     const totalAmountCalc = basketItems.reduce((acc: number, cur: IBasketItem, index: number) => {
@@ -13,16 +14,18 @@ const BasketComponent = () => {
         acc = cur.odd * acc * STAKE;
       } else {
         acc = cur.odd * acc;
-
       }
 
       return acc;
     }, 1);
     setTotalAmount(totalAmountCalc.toFixed(2));
+    if (basketSectionRef.current) {
+      basketSectionRef.current.scrollTo(0, basketSectionRef.current.scrollHeight);
+    }
   }, [basketItems]);
 
   return (
-    <section className={'basket'}>
+    <section className={'basket'} ref={basketSectionRef}>
       <div className={'odds'}>
         {basketItems.map((item: IBasketItem) => (
           <p key={item.id}>
